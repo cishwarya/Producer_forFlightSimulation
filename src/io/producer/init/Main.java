e701a4958acc1c0b5c4680edaead1b0c4f0b01da
Thishanth <thish6@gmail.com>
2017-06-16 00:37:56 +0200

    todo added

--- src/io/producer/init/Main.java
+++ src/io/producer/init/Main.java
@@ -14,7 +14,12 @@ public class Main {
 		
 		rows.forEach(row -> {
 			System.out.println(row.get(0));
-		});
+			
+			//Thread.sleep(2second);
+			//Change row to java object
+			//change java object to json object
+			//send post request to flightlog server
+		});		
 	}
 
 }
