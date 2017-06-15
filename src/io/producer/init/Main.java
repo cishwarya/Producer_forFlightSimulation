f1607b29ad14ed3ade66b57f99f7d3d34c3fee7a
Thishanth <thish6@gmail.com>
2017-06-16 00:08:16 +0200

    java 8 loop

--- src/io/producer/init/Main.java
+++ src/io/producer/init/Main.java
@@ -1,12 +1,20 @@
 package io.producer.init;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.producer.file.ReadFile;
 
 public class Main {
-
+	
+	private static List<List<String>> rows = new ArrayList<List<String>>();
        
 	public static void main(String[] args) {
-		ReadFile.read();
+		rows = ReadFile.read();
+		
+		rows.forEach(row -> {
+			System.out.println(row.get(0));
+		});
 	}
 
 }
